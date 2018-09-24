fd81fad5d03d93c43a5f3861141198c8ad6b6318
frankGz <1069457689@qq.com>
2018-09-24 00:17:34 -0400

    show max in c exhau
    also cheatsheet

--- src/foundation/C_Exhaustive.java
+++ src/foundation/C_Exhaustive.java
@@ -1,5 +1,9 @@
 package foundation;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import util.CryptoTools;
 
 public class C_Exhaustive {
@@ -7,6 +11,7 @@ public class C_Exhaustive {
 	public static void main(String[] args) throws Exception {
 		byte[] ct = CryptoTools.fileToBytes("data/MSG2.ct");
 		byte[] test = new byte[ct.length];
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 		
 		int[] frequences;
 		int i,j,k;
@@ -26,7 +31,11 @@ public class C_Exhaustive {
 			}
 			
 			System.out.println((char) (i+'A') + " -->" + sum);
+			map.put(i, sum);
 			//largest is the shift
 		}
+		
+		Entry<Integer, Integer> maxEntry = map.entrySet().stream().max(Map.Entry.comparingByValue()).get();
+		System.out.println("Max is: " + (char)(maxEntry.getKey() + 'A') + " --> " + maxEntry.getValue());
 	}
 }
